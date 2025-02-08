4f72ab25f2a5b63581ac9788824b668741e00bb7
lloyd72 <122834454+lloyd72@users.noreply.github.com>
2025-02-08 14:32:46 +0000

    Add files via upload

---
+++ includes/HTML-head.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content="A GCTU Forum" />
+        <meta name="keywords" content=" The Student Parlor" >
+  
+        <title><?php echo TITLE; ?></title>
+        
+        <link href="css/bootstrap.min.css" rel="stylesheet"> 
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"> 
+        <link href="css/footer.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
+        
+        
+        
+        <link rel="shortcut icon" href="img/logo3.png" />
